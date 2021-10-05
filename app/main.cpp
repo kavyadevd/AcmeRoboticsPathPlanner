dbc3ca08de8ae74f0a8866dee4f96b3b14d09e7b
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-05 19:48:11 -0400

    Add main file, header file and cmake files

---
+++ app/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Kavyashree Devadiga (kavya@umd.edu) - Driver, Aswath Muthuselvam (aswath@umd.edu) - Navigator
+ * @brief A program for arm manipulator path planning
+ * @version 0.1
+ * @date 2021-10-05
+ * @copyright All rights reserved 
+ * 
+ */
+
+int main() {
+    return 0;
+}
---
+++ include/solver.h
@@ -0,0 +1,17 @@
+/**
+ * @file solver.h
+ * @author Kavyashree Devadiga (kavya@umd.edu) - Driver, Aswath Muthuselvam (aswath@umd.edu) - Navigator
+ * @brief The header file for arm manipulator path planning IK solver
+ * @version 0.1
+ * @date 2021-09-30
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef ACMEROBOTICSPATHPLANNER_INCLUDE_SOLVER_H_  // NOLINT
+#define ACMEROBOTICSPATHPLANNER_INCLUDE_SOLVER_H_
+
+class Solver{
+};
+
+#endif  // ACMEROBOTICSPATHPLANNER_INCLUDE_SOLVER_H_  // NOLINT
