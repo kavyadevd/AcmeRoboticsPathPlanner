606d2e8c8b958fcfb3988c5bbfdb79ed955cdda8
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-06 16:23:48 -0400

    Add state class header and code file

---
+++ app/state.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file state.cpp
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @brief File for arm manipulator state definitions
+ * @version 0.1
+ * @date 2021-10-05
+ * @copyright All rights reserved 
+ * 
+ */
+
+
+#include "state.h"     // NOLINT
+
+State::State() {
+}
---
+++ include/state.h
@@ -0,0 +1,34 @@
+/**
+ * @file state.h
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @brief The header file for coordinate system used in the project
+ * @version 0.1
+ * @date 2021-10-04
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef STATE_H_  // NOLINT
+#define STATE_H_
+
+class State {
+ public:
+    double x;               ///< Variable to store the position x-coordinate
+    double y;               ///< Variable to store the position y-coordinate
+    double z;               ///< Variable to store the position z-coordinate
+    double theta;           ///< Variable to store position position theta value
+    double phi;             ///< Variable to store tposition position phi value
+    double alpha;             ///< Variable to store position position alpha value
+
+    double velocity;        ///< Variable to store the robot velocity
+    double acceleration;    ///< Variable to store the robot acceleration
+    double torque;          ///< Variable to store the robot torque
+
+/**
+* @brief Construct a new state object. Sets default values to all attributes
+*/
+    State();
+};
+
+
+#endif  // STATE_H_  // NOLINT
