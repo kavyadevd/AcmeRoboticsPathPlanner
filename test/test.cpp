2a55b1cf5303dd5880fbb34fcc8ff8d867f80b71
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-05 23:15:02 -0400

    Add test case for defined functions

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,17 @@
+/**
+ * @file test.cpp
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @brief File for running gtests
+ * @version 0.1
+ * @date 2021-10-05
+ * @copyright All rights reserved 
+ * 
+ **/
+
 #include <gtest/gtest.h>
+#include "solver.h"  // NOLINT
 
 TEST(TestStud, should_pass_solver) {
-  EXPECT_EQ(1, 1);
+  Solver ik_solver;
+  EXPECT_EQ(ik_solver.solve(), 1);
 }
