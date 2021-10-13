df56bfc382a1ff4ba08ab9ca8247af2d47fcd06f
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-12 20:14:44 -0400

    Add simulation implementation file

---
+++ app/simulator.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file simulator.cpp
+ * @author Kavyashree Devadiga (kavya@umd.edu) - Driver, Aswath Muthuselvam (aswath@umd.edu) - Navigator
+ * @brief A program for result simulation implementation
+ * @version 0.1
+ * @date 2021-10-11
+ * @copyright All rights reserved 
+ * 
+ */
+
+#include <iostream>
+#include "simulator.h"  // NOLINT
+
+bool Simulator::Initialize() {
+    return true;
+}
