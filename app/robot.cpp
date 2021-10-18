951c96ef48c8f4973d9fa076b8ff0213df427ff6
aswath8 <m.aswath08@mail.com>
2021-10-18 10:46:37 -0400

    Task 2.2 Add headers for simulator class
    Task 2.2: As per class depencdency, simulator class methods will be
    used by Robot class to access simulation information like joints.
    The methods will be moved from main.cpp in the next commit
     into Initialize() function later.

--- app/robot.cpp
+++ app/robot.cpp
@@ -1,35 +1,31 @@
 /**
  * @file robot.cpp
- * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam
+ * (aswath@umd.edu)
  * @brief File for robot class method definitions
  * @version 0.1
  * @date 2021-10-05
- * @copyright All rights reserved 
- * 
+ * @copyright All rights reserved
+ *
  */
 
+#include "robot.h"  // NOLINT
+#include <iostream>
+#include "simulator.h"
 
-#include "robot.h"     // NOLINT
+using std::cout;
+using std::endl;
 
-Robot::Robot() {
-}
+Robot::Robot() {}
 
 bool Robot::Initialize() {
-    return true;
+  return true;
 }
 
-bool Robot::Solver() {
-    return true;
-}
+bool Robot::Solver() { return true; }
 
-bool Robot::TrajectoryPlanner() {
-    return true;
-}
+bool Robot::TrajectoryPlanner() { return true; }
 
-bool Robot::Controller() {
-    return true;
-}
+bool Robot::Controller() { return true; }
 
-bool Robot::Simulate() {
-    return true;
-}
+bool Robot::Simulate() { return true; }
