430282057b8a02e415304a7ca5e02c8a786015b3
aswath8 <m.aswath08@mail.com>
2021-10-19 00:35:38 -0400

    Task 2.13 Added Test case for simulator.cpp methods
    Task 2.13 The methods of simulator.cpp are called and tested against
    valid integer values. These methods communicates with the CoppeliaSim
    simulation environment. Tests will fail if the simulation software is not
    open.

--- test/test.cpp
+++ test/test.cpp
@@ -1,22 +1,24 @@
 /**
  * @file test.cpp
- * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam
+ *(aswath@umd.edu)
  * @brief File for running gtests
  * @version 0.1
  * @date 2021-10-05
- * @copyright All rights reserved 
- * 
+ * @copyright All rights reserved
+ *
  **/
 
 #include <gtest/gtest.h>
-#include "solver.h"  // NOLINT
-#include "state.h"  // NOLINT
-#include "robot.h"  // NOLINT
+
+#include "robot.h"      // NOLINT
 #include "simulator.h"  // NOLINT
+#include "solver.h"     // NOLINT
+#include "state.h"      // NOLINT
 
 /**
  * @brief Tests the default constructor
-*/
+ */
 TEST(TestIKStub, test_the_default_constructor) {
   Solver ik_solver;
   EXPECT_EQ(0, 0);
@@ -24,7 +26,7 @@ TEST(TestIKStub, test_the_default_constructor) {
 
 /**
  * @brief Tests IK solver stub
-*/
+ */
 TEST(TestIKStub, should_pass_solver) {
   Solver ik_solver;
   EXPECT_EQ(ik_solver.PerformIK(), 0);
@@ -32,7 +34,7 @@ TEST(TestIKStub, should_pass_solver) {
 
 /**
  * @brief Tests Set Error stub
-*/
+ */
 TEST(TestIKStub, set_error) {
   Solver ik_solver;
   EXPECT_EQ(ik_solver.SetErrorTolerance(0.007), true);
@@ -40,7 +42,7 @@ TEST(TestIKStub, set_error) {
 
 /**
  * @brief Tests Get Error stub
-*/
+ */
 TEST(TestIKStub, get_error) {
   Solver ik_solver;
   EXPECT_EQ(ik_solver.GetErrorTolerance(), 0.5);
@@ -48,16 +50,15 @@ TEST(TestIKStub, get_error) {
 
 /**
  * @brief Tests error tolance check stub
-*/
+ */
 TEST(TestIKStub, is_error_tolerable) {
   Solver ik_solver;
   EXPECT_EQ(ik_solver.IsErrorTolerable(0.0001), true);
 }
 
-
 /**
  * @brief Tests State default constructor check stub
-*/
+ */
 TEST(TestIKStub, test_state_object) {
   State state_;
   EXPECT_EQ(0, 0);
@@ -65,7 +66,7 @@ TEST(TestIKStub, test_state_object) {
 
 /**
  * @brief Tests robot default constructor
-*/
+ */
 TEST(TestIKStub, test_robot_object) {
   Robot robotic_arm;
   EXPECT_EQ(true, true);
@@ -73,7 +74,7 @@ TEST(TestIKStub, test_robot_object) {
 
 /**
  * @brief Tests robot initialize method
-*/
+ */
 TEST(TestIKStub, test_robot_initialize) {
   Robot robotic_arm;
   EXPECT_EQ(robotic_arm.Initialize(), true);
@@ -81,7 +82,7 @@ TEST(TestIKStub, test_robot_initialize) {
 
 /**
  * @brief Tests robot solve method
-*/
+ */
 TEST(TestIKStub, test_solver) {
   Robot robotic_arm;
   EXPECT_EQ(robotic_arm.Solver(), true);
@@ -89,7 +90,7 @@ TEST(TestIKStub, test_solver) {
 
 /**
  * @brief Tests robot Trajectory planner
-*/
+ */
 TEST(TestIKStub, test_trajectory_planner) {
   Robot robotic_arm;
   EXPECT_EQ(robotic_arm.TrajectoryPlanner(), true);
@@ -97,7 +98,7 @@ TEST(TestIKStub, test_trajectory_planner) {
 
 /**
  * @brief Tests robot controller method
-*/
+ */
 TEST(TestIKStub, test_controller) {
   Robot robotic_arm;
   EXPECT_EQ(robotic_arm.Controller(), true);
@@ -105,8 +106,20 @@ TEST(TestIKStub, test_controller) {
 
 /**
  * @brief Tests robot simulate method
-*/
+ */
 TEST(TestIKStub, test_simulate) {
   Robot robotic_arm;
   EXPECT_EQ(robotic_arm.Simulate(), true);
+}
+
+/**
+ * @brief Tests simulator class
+ */
+TEST(TestSimulatorStub, test_simulator) {
+  Simulator simulator;
+  EXPECT_EQ(simulator.Initialize(), 1);
+  char* joint = "UR5_joint1";
+  simxInt handle = simulator.GetObjectHandle(joint);
+  EXPECT_NE(simulator.GetParent(handle), -1);
+  EXPECT_NE(simulator.GetChild(handle, 0), -1);
 }
