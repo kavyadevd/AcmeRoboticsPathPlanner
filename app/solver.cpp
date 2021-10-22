fa46d595e89fdf6751629cb4ac3c267d12d27d9a
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-22 17:01:09 -0400

    Add notation information to file description

--- app/solver.cpp
+++ app/solver.cpp
@@ -2,6 +2,20 @@
  * @file solver.cpp
  * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
  * @brief File for arm manipulator path planning solver definitions
+ * 
+ * Note: Transformation matrix notations are as follows:
+ *  0                               |06R    0P6|
+ *      T(θ1,θ2,θ3,θ4,θ5,θ6) =      |          |
+ *  6                               |0      1  |
+ * 
+ *                                  |0X6x   0Y6x     0Z6x    0P6x|
+ *                                  |0X6y   0Y6y     0Z6y    0P6y|
+ *                            =     |0X6z   0Y6z     0Z6z    0P6z|
+ *                                  |  0     0        0        0 |
+ * 
+ * 
+ * 
+ * 
  * @version 0.1
  * @date 2021-10-05
  * @copyright All rights reserved 
@@ -35,7 +49,7 @@ bool Solver::IsErrorTolerable(double _error) {
 
 /**
  * @brief Fetch angle for wrist frame in relation to the base frame
- * θ1 = atan2(0T5y,0T5x)+- acos[ d4 / (√ 0T5x^2  + 0T5y^2) ]
+ * θ1 = atan2(0P5y,0P5x)+- acos[ d4 / (√ 0P5x^2  + 0P5y^2) ]
  * 
  * @return double 
  */
@@ -45,7 +59,7 @@ double GetTheta_1() {
 
 /**
  * @brief Fetch angle for link 5 
- * θ5 = acos [ (0T6x sinθ1 - 0T6y cosθ1 - d4) / d6 ]
+ * θ5 = acos [ (0P6x sinθ1 - 0P6y cosθ1 - d4) / d6 ]
  * 
  * @return double 
  */
