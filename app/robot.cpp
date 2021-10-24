37ddbc20126d13125aa6f742227bd0d301d2bb80
aswath8 <m.aswath08@mail.com>
2021-10-24 01:27:04 -0400

    Task 2.8 Get values of robot joint and links
    Task 2.8 Add values of robot joint and links. Add method to robot.cpp
    to get object position wrt to another object. Add eigen Matrix to the cpp file
    to store Transformations.

--- app/robot.cpp
+++ app/robot.cpp
@@ -12,17 +12,27 @@
 #include "robot.h"  // NOLINT
 
 #include <unistd.h>
+#include <Eigen/Dense>
+#include <cmath>
+
 
 #include <iostream>
+#include <vector>
 
 #include "simulator.h"  // NOLINT
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::vector;
+using namespace Eigen;
 
 Robot::Robot() {}
 
 bool Robot::Initialize(Simulator* simulator) {
+  this->simulator = simulator;
+
+  char name_floor[] = "Floor";
   char name_joint1[] = "UR5_joint1";
   char name_joint2[] = "UR5_joint2";
   char name_joint3[] = "UR5_joint3";
@@ -37,12 +47,13 @@ bool Robot::Initialize(Simulator* simulator) {
   char name_link5[] = "UR5_link5";
   char name_link6[] = "UR5_link6";
 
-  simxInt j1 = simulator->GetObjectHandle(name_joint1);
-  simxInt j2 = simulator->GetObjectHandle(name_joint2);
-  simxInt j3 = simulator->GetObjectHandle(name_joint3);
-  simxInt j4 = simulator->GetObjectHandle(name_joint4);
-  simxInt j5 = simulator->GetObjectHandle(name_joint5);
-  simxInt j6 = simulator->GetObjectHandle(name_joint6);
+  simxInt floorh = simulator->GetObjectHandle(name_floor);
+  j1 = simulator->GetObjectHandle(name_joint1);
+  j2 = simulator->GetObjectHandle(name_joint2);
+  j3 = simulator->GetObjectHandle(name_joint3);
+  j4 = simulator->GetObjectHandle(name_joint4);
+  j5 = simulator->GetObjectHandle(name_joint5);
+  j6 = simulator->GetObjectHandle(name_joint6);
 
   simxInt l1 = simulator->GetObjectHandle(name_link1);
   simxInt l2 = simulator->GetObjectHandle(name_link2);
@@ -60,6 +71,31 @@ bool Robot::Initialize(Simulator* simulator) {
   float T05[12];
   float T06[12];
 
+  simxFloat pos[3];
+
+  //GetEndEffectorPosition();
+  simulator->GetObjectPosition(j1, j1, pos);
+   cout << "\nPosition of 1 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j2, j1, pos);
+   cout << "\nPosition of 2 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j3, j1, pos);
+   cout << "\nPosition of 3 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j4, j1, pos);
+   cout << "\nPosition of 4 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j5, j1, pos);
+   cout << "\nPosition of 5 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j6, j1, pos);
+   cout << "\nPosition of 6 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  int k;
+  cin>>k;
+
+/*
   cout << "\n------Joint matrix--------";
   simulator->GetJointMatrix(j1, T01);
   simulator->GetJointMatrix(j2, T02);
@@ -76,22 +112,7 @@ bool Robot::Initialize(Simulator* simulator) {
   simulator->GetJointMatrix(l5, T05);
   simulator->GetJointMatrix(l6, T06);
 
-  cout << "Starting Froward Kinematics Demo" << endl;
-  simxSetJointTargetPosition(simulator->GetClientID(), j1, 0.0,
-                             simx_opmode_blocking);
-  simxSetJointTargetPosition(simulator->GetClientID(), j1, 18.13,
-                             simx_opmode_blocking);
-  sleep(1);
-  simxSetJointTargetPosition(simulator->GetClientID(), j2, 0.6,
-                             simx_opmode_blocking);
-  simxSetJointTargetPosition(simulator->GetClientID(), j3, 0.6,
-                             simx_opmode_blocking);
-  simxSetJointTargetPosition(simulator->GetClientID(), j4, 0.6,
-                             simx_opmode_blocking);
-  simxSetJointTargetPosition(simulator->GetClientID(), j5, 1.2,
-                             simx_opmode_blocking);
-  simxSetJointTargetPosition(simulator->GetClientID(), j6, 1.2,
-                             simx_opmode_blocking);
+  //ForwardKinematics(1, 0, 0, 0, 0, 0);
 
   cout << "\n------Joint matrix--------" << endl;
   simulator->GetJointMatrix(j1, T01);
@@ -108,14 +129,85 @@ bool Robot::Initialize(Simulator* simulator) {
   simulator->GetJointMatrix(l4, T04);
   simulator->GetJointMatrix(l5, T05);
   simulator->GetJointMatrix(l6, T06);
+*/
+  //&pos = 1;
+  //simulator->SetJointPosition(j1, pos);
+  //cout << pos[0];
 
-  simxFloat* pos;
-  *pos = 1;
+  simulator->SetJointTargetAngle(j1, 0.3);
+  simulator->SetJointTargetAngle(j2, 0.3);
+  simulator->SetJointTargetAngle(j3, 0.3);
+
   sleep(1);
-  simulator->GetJointMatrix(j1, T01);
-  simulator->SetJointPosition(j1, pos);
-  cout << pos[0];
+
+  //GetEndEffectorPosition();
+  simulator->GetObjectPosition(j1, j1, pos);
+   cout << "\nPosition of 1 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j2, j1, pos);
+   cout << "\nPosition of 2 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j3, j1, pos);
+   cout << "\nPosition of 3 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j4, j1, pos);
+   cout << "\nPosition of 4 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j5, j1, pos);
+   cout << "\nPosition of 5 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  simulator->GetObjectPosition(j6, j1, pos);
+   cout << "\nPosition of 6 wrt to 1:\n"
+       << pos[0] << " " << pos[1] << " " << pos[2]<<endl;
+  cin>>k;
+
   return true;
+}
+
+void Robot::ForwardKinematics(float t1, float t2, float t3, float t4, float t5,
+                              float t6) {
+  cout << "Starting Froward Kinematics" << endl;
+  simulator->SetJointTargetAngle(j1, t1);
+  simulator->SetJointTargetAngle(j2, t2);
+  simulator->SetJointTargetAngle(j3, t3);
+  simulator->SetJointTargetAngle(j4, t4);
+  simulator->SetJointTargetAngle(j5, t5);
+  simulator->SetJointTargetAngle(j6, t6);
+
+  //------------1 to 2------------//
+  
+
+}
+
+VectorXf Robot::GetEndEffectorPosition() {
+  cout << "Starting Froward Kinematics" << endl;
+  
+  vector<double> a;
+  vector<double> d;
+  vector<double> al;
+  vector<double> t;
+  a.push_back(0.2);
+  d.push_back(0.3);
+  t.push_back(0.6);
+  ///Set transformations matrix
+  Matrix4f transformation;
+  int i=0;
+  //for(int i = 0; i < 5; i++){
+    transformation << cos(t[i]) , sin(t[i]) , 0 , a[i] ,
+    sin(t[i]) , cos(t[i]) , 0 , 0,
+    0 , 0 , 1 , d[i],
+    0 , 0 , 0 , 1 ;
+  //}
+
+  cout<<"Transformation matrix: "<<endl;
+    Matrix4f tr = transformation;
+    cout<< tr(0,1)<< "  "<< tr(0,2);
+  
+  Matrix4f tf06;
+
+  VectorXf v(3);
+  //v << tf06(0,3) , tf06(1,3) , tf06(2,3) ;
+  //return v;
 }
 
 bool Robot::Solver() { return true; }
--- include/robot.h
+++ include/robot.h
@@ -1,62 +1,89 @@
 /**
  * @file robot.h
- * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam (aswath@umd.edu)
+ * @author Kavyashree Devadiga (kavya@umd.edu), Aswath Muthuselvam
+ * (aswath@umd.edu)
  * @brief The header file for coordinate system used in the project
  * @version 0.1
  * @date 2021-10-04
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #ifndef ROBOT_H_  // NOLINT
 #define ROBOT_H_
 
-#include "state.h"  // NOLINT
 #include "simulator.h"  // NOLINT
+#include "state.h"      // NOLINT
+#include <Eigen/Dense>
 
-class Robot : public State{  // Robot inherits State class public members
+using namespace Eigen;
+
+
+class Robot : public State {  // Robot inherits State class public members
  public:
-    /**
-    * @brief Construct a new robot object. Sets default values to all attributes
-    */
-        Robot();
+  /**
+   * @brief Construct a new robot object. Sets default values to all attributes
+   */
+  Robot();
 
-    /**
-    * @brief Method will set the environment variables
-    * @return bool flag indicating successful operation
-    */
-    bool Initialize(Simulator* simulator);
+  /**
+   * @brief Method will set the environment variables
+   * @param Simulator Use the simulator object to extract information about the
+   * robot from CoppeliaSim.
+   * @return bool flag indicating successful operation
+   */
+  bool Initialize(Simulator* simulator);
 
-    /**
-    * @brief Method will set the environment variables
-    * @return bool flag indicating successful operation
-    */
-    bool Solver();
+  /**
+   * @brief Method will set the environment variables
+   * @return bool flag indicating successful operation
+   */
+  bool Solver();
 
-    /**
-    * @brief Method will set the solve for trajectory
-    * @return bool flag indicating successful operation
-    */
-    bool TrajectoryPlanner();
+  /**
+   * @brief Method will set the solve for trajectory
+   * @return bool flag indicating successful operation
+   */
+  bool TrajectoryPlanner();
 
-    /**
-    * @brief Method collects waypoints and feeds successive points for simulation
-    * @return bool flag indicating successful operation
-    */
-    bool Controller();
+  /**
+   * @brief Method collects waypoints and feeds successive points for simulation
+   * @return bool flag indicating successful operation
+   */
+  bool Controller();
 
-    /**
-    * @brief Method to invoke simulator with desired joint actions
-    * @return bool flag indicating successful operation
-    */
-    bool Simulate();
+  /**
+   * @brief Method to invoke simulator with desired joint actions
+   * @return bool flag indicating successful operation
+   */
+  bool Simulate();
+
+  /**
+   * @brief Method to execute forward kinematics given the joint angles.
+   * @param t1 Angle in radians of joint 1.
+   * @param t2 Angle in radians of joint 2.
+   * @param t3 Angle in radians of joint 3.
+   * @param t4 Angle in radians of joint 4.
+   * @param t5 Angle in radians of joint 5.
+   * @param t6 Angle in radians of joint 6.
+   */
+  void ForwardKinematics(float t1, float t2, float t3, float t4, float t5,
+                         float t6);
+
+  /**
+   * @brief Method to execute forward kinematics given the joint angles.
+   * @return Vector of size 3, containing x,y,z values of end effector.
+   */
+  VectorXf GetEndEffectorPosition();
+
+ private:
+  Simulator* simulator;  ///< Reference object to Simulator class object.
+  simxInt j1;            ///< Joint 1 handle reference.
+  simxInt j2;            ///< Joint 2 handle reference.
+  simxInt j3;            ///< Joint 3 handle reference.
+  simxInt j4;            ///< Joint 4 handle reference.
+  simxInt j5;            ///< Joint 5 handle reference.
+  simxInt j6;            ///< Joint 6 handle reference.
 };
-
-
-
-
-
-
-
 
 #endif  // ROBOT_H_  // NOLINT
