289704cd1900398f97d9f27d082237235e3f04d6
aswath8 <m.aswath08@mail.com>
2021-10-24 01:40:34 -0400

    Task 2.5 Added method to retrieve object position
    Task 2.5 Added method to simulator.h and simulator.cpp to retrieve
    given object's position wrt another object in CoppeliaSim.

--- include/simulator.h
+++ include/simulator.h
@@ -39,7 +39,7 @@ class Simulator {
 
   /**
    * @brief Returns client ID of our c++ program assigned by CoppeliaSim.
-   * 
+   *
    * @return int returns active CoppeliaSim communication unique ID.
    */
   int GetClientID();
@@ -47,15 +47,16 @@ class Simulator {
   /**
    * @brief Gets robot's joint matrix of specified joint.
    *
-   * @param Joint reference of robot arm as int .
-   * @param Matrix variable in which the joint matrix is filled by CoppeliaSim.
+   * @param joint Joint reference of robot arm as int .
+   * @param matrix Matrix variable in which the joint matrix is filled by
+   * CoppeliaSim.
    */
   void GetJointMatrix(simxInt joint, float* matrix);
 
   /**
    * @brief Get the integer reference of an object given the object's name.
    *
-   * @param The name of the object.
+   * @param name The name of the object.
    * @return Integer reference to the object.
    */
   simxInt GetObjectHandle(char* name);
@@ -63,7 +64,7 @@ class Simulator {
   /**
    * @brief Get Parent object from CoppeliaSim with remoteX API.
    *
-   * @param Integer reference of Handle(arm joint) to get it's parent.
+   * @param handle Integer reference of Handle(arm joint) to get it's parent.
    * @return Integer reference of parent object.
    */
   simxInt GetParent(simxInt handle);
@@ -71,20 +72,46 @@ class Simulator {
   /**
    * @brief Get child object from CoppeliaSim with remoteX API.
    *
-   * @param Integer reference of the parent object.
-   * @param To obtain the nth child from current parent object.
+   * @param parentObjectHandle Integer reference of the parent object.
+   * @param childIndex To obtain the nth child from current parent object.
    * @return Integer reference of child object.
    */
   simxInt GetChild(simxInt parentObjectHandle, simxInt childIndex);
 
   /**
-   * @brief Set the joint angle of robotic arm with CoppeliaSim with remoteX API.
+   * @brief Set the joint angle of robotic arm with CoppeliaSim with remoteX
+   * API.
    *
-   * @param Integer reference of the parent object.
-   * @param Position of the arm to rotate(angular degrees or prismatic length).
+   * @param jointHandle Integer reference of the parent object.
+   * @param position Position of the arm to rotate(angular degrees or prismatic
+   * length).
    * @return Returns operation status.
    */
   simxInt SetJointPosition(simxInt jointHandle, simxFloat* position);
+
+  /**
+   * @brief Sends command to CoppeliaSim's arm controller to move the arm to set
+   * position.
+   *
+   * @param jointHandle Integer reference of the joint.
+   * @param angle Angle of the arm to rotate(angular degrees or prismatic
+   * length).
+   * @return Returns operation status.
+   */
+  simxInt SetJointTargetAngle(simxInt jointHandle, simxFloat angle);
+
+  /**
+   * @brief Gets the position of object position.
+   *
+   * @param objectHandle Integer reference of the joint.
+   * @param relativeObjectHandle Integer reference of the joint with respect
+   * to which position needs to be calculated.
+   * @param position Float array where the 3 output values(x,y,z) will be
+   * stored.
+   * @return Returns operation status.
+   */
+  simxInt GetObjectPosition(simxInt objectHandle, simxInt relativeObjectHandle,
+                            simxFloat* position);
 
   /**
    * @brief Desctructor for Simulator class, calls Simulator::Stop()
