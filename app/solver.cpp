c55c5454fe872b5f69e3e98a4a9e8b70476c27d4
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-25 01:03:38 -0400

    Create robot object handles

--- app/solver.cpp
+++ app/solver.cpp
@@ -8,7 +8,7 @@
  * @copyright  All rights reserved
  * Note: Transformation matrix notations are as follows:
  *  0                               |06R    0P6|
- *      T(θ1,θ2,θ3,θ4,θ5,θ6) =      |          |
+ *      T(θ1, θ2, θ3, θ4, θ5, θ6) =      |          |
  *  6                               |0      1  |
  * 
  *                                  |0X6x   0Y6x     0Z6x    0P6x|
@@ -18,16 +18,48 @@
  * 
  */
 
-
 #include<math.h>
+#include<iostream>
+#include "ik.h"
+#include "environment.h"
 #include "solver.h"     // NOLINT
+#include "robot.h"     // NOLINT
+#include "simulator.h"
+
+extern "C" {
+    #include "extApi.h"
+}
 
 Solver::Solver() {
     error_tolerance = 0.5;
 }
 
-int Solver::PerformIK() {
+int Solver::PerformIK(int client_ID) {
+    try {
+    std::cout<< "created env start ";
+    // Create an environment for IK solver
+    ikCreateEnvironment();
+    std::cout<< "created env end";
+          int arm_handles[7];
+        ikGetObjectHandle("UR5_joint1", arm_handles+0);
+        ikGetObjectHandle("UR5_joint2", arm_handles+1);
+        ikGetObjectHandle("UR5_joint3", arm_handles+2);
+        ikGetObjectHandle("UR5_joint4", arm_handles+3);
+        ikGetObjectHandle("UR5_joint5", arm_handles+4);
+        ikGetObjectHandle("UR5_joint6", arm_handles+5);
+        int model1TargetHandle;
+        ikGetObjectHandle("UR5_joint6", &model1TargetHandle);
+        int model1BaseHandle;
+        ikGetObjectHandle("UR5_joint5", &model1BaseHandle);
+
+        simReal v = 0.0;
+
+
     return 0;
+    } catch (const char* msg) { /* catch exception if any */
+        std::cout << "Exception occurred" << std::endl;
+        return 0;
+  }
 }
 
 bool Solver::SetErrorTolerance(double _error) {
@@ -59,7 +91,7 @@ void GetTransformationMatrix() {
  *       6      |-d6|
  *              | 1 |
  * 
- * θ1 = atan2(0P5y,0P5x)+- acos[ d4 / (√ 0P5x^2  + 0P5y^2) ]
+ * θ1 = atan2(0P5y, 0P5x)+- acos[ d4 / (√ 0P5x^2  + 0P5y^2) ]
  * 
  * @return double 
  */
