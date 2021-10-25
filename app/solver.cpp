212d39d88661e7254c4c2f84315438acd185754f
Kavyashree <kavya27.devadiga@gmail.com>
2021-10-25 10:58:24 -0400

    Remove IK API code
    Not required for Jacobian method

--- app/solver.cpp
+++ app/solver.cpp
@@ -20,8 +20,6 @@
 
 #include<math.h>
 #include<iostream>
-#include "ik.h"    // NOLINT
-#include "environment.h"    // NOLINT
 #include "solver.h"     // NOLINT
 
 extern "C" {
@@ -33,59 +31,7 @@ Solver::Solver() {
 }
 
 int Solver::PerformIK(int client_ID, double goal_x, double goal_y, double goal_z) {
-    try {
-    std::cout<< "created env start ";
-    // Create an environment for IK solver
-    ikCreateEnvironment();
-    std::cout<< "created env end";
-
-    int arm_handles[7];
-    simReal sim_math_real = 0.0;
-    C7Vector initial_transf;
-    float simulationStep;
-
-    ikGetObjectHandle("UR5_joint1", arm_handles+0);
-    ikGetObjectHandle("UR5_joint2", arm_handles+1);
-    ikGetObjectHandle("UR5_joint3", arm_handles+2);
-    ikGetObjectHandle("UR5_joint4", arm_handles+3);
-    ikGetObjectHandle("UR5_joint5", arm_handles+4);
-    ikGetObjectHandle("UR5_joint6", arm_handles+5);
-    int target_robot_handle;
-    ikGetObjectHandle("UR5_joint6", &target_robot_handle);
-    int base_robot_handle;
-    ikGetObjectHandle("UR5_joint5", &base_robot_handle);
-
-    ikGetObjectTransformation(target_robot_handle, base_robot_handle, &initial_transf);
-
-    C7Vector tr(C4Vector(0, 0, 0), C3Vector::zeroVector);
-    tr.X = C3Vector(goal_x, goal_y, goal_z);
-    // Slow down the simulation
-    simxUChar simWaitingForTrigger = 0;
-    while ( (simWaitingForTrigger == 0) )
-        simxGetBooleanParameter(client_ID, sim_boolparam_waiting_for_trigger,
-        &simWaitingForTrigger, simx_opmode_blocking);
-
-    simxGetFloatingParameter(client_ID, sim_floatparam_simulation_time_step,
-    &simulationStep, simx_opmode_buffer);
-    sim_math_real += simReal(0.6)*simReal(simulationStep);
-
-    initial_transf.X = C3Vector(simReal(-0.3-cos(sim_math_real)*0.1),
-     simReal(sin(sim_math_real)*0.1), simReal(0.629+sin(sim_math_real*9)*0.01));
-    ikSetObjectTransformation(target_robot_handle, base_robot_handle, &initial_transf);
-
-    ikHandleIkGroup(ik_handle_all);     // API call for IK solver
-
-    simxPauseCommunication(client_ID, 1);
-
-    simReal pos;
-    ikGetJointPosition(arm_handles[0], &pos);
-
-    int r = simx_return_remote_error_flag;  // Variable to store error flag
-    while (r == simx_return_remote_error_flag)
-        r = simxSynchronousTrigger(client_ID);  // Trigger next simulation step
-
-    ikEraseEnvironment();
-    ikEraseEnvironment();
+    try {    
     return 0;
     } catch (const char* msg) { /* catch exception if any */
         std::cout << "Exception occurred" << std::endl;
