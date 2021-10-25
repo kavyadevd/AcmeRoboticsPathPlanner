1b4074c52a6ee8c4a5fe33d00581f08ea5192bcb
Kavya Devadiga <kavya27.devadiga@gmail.com>
2021-10-25 01:04:04 -0400

    Invoke IK solver method

--- app/main.cpp
+++ app/main.cpp
@@ -12,22 +12,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include <iostream>
-
-#include "robot.h"
-#include "simulator.h"
+#include "robot.h"        // NOLINT
+#include "simulator.h"  // NOLINT
+#include "solver.h"  // NOLINT
 
 using std::cout;
 using std::endl;
 
 int main() {
+  try {
+  Solver solve_ik;
   Simulator simulator;
   Robot robot;
   if (!simulator.Initialize()) {
+        std::cout << "Exception occurred Initialize failed" << std::endl;
     return -1;
   }
-  robot.Initialize(&simulator);
-  
+  sleep(3);
+  int client_ID = simulator.GetClientID();
+  std::cout << client_ID << std::endl;
+  solve_ik.PerformIK(client_ID);
   return 0;
+  } catch (const char* msg) { /* catch exception if any */
+        std::cout << "Exception occurred" << std::endl;
+        return 0;
+  }
 }
