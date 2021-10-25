4e72c60de8798766ac73531aad05e3659eeff0d2
aswath8 <m.aswath08@mail.com>
2021-10-25 12:05:11 -0400

    Task 2.6 Continous movement of robotic arm form one point to another
    Task 2.6 First working commit of the robotic arm moving from one point to the other.
    dx=0, dy=0.001, dz=-0.001. The arm moves continuously forward and down, during someruns,
    it encounters singularity and takes a whole spin to reach the same point.

--- app/robot.cpp
+++ app/robot.cpp
@@ -19,19 +19,21 @@
 #include <vector>
 
 #include "simulator.h"  // NOLINT
+#include "solver.h" //NOLINT
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
-using namespace Eigen;
+using Eigen::Matrix4d;
+using Eigen::MatrixXd;
 
 Robot::Robot() {}
 
 bool Robot::Initialize(Simulator* simulator) {
+  State state;
   try {
     this->simulator = simulator;
-
     ///< Variable to store origin
     char origin_name[] = "Floor";
 
@@ -50,8 +52,6 @@ bool Robot::Initialize(Simulator* simulator) {
       joint_handle.push_back(simulator->GetObjectHandle(joint_names[it]));
       link_handle.push_back(simulator->GetObjectHandle(link_names[it]));
 
-
-
       //cout << "\n------Joint matrix " << it + 1 << " --------\n";
       //float* joint_ptr = new float[12];
       //joint_matrix.push_back(joint_ptr);
@@ -62,19 +62,22 @@ bool Robot::Initialize(Simulator* simulator) {
       // link_matrix.push_back(link_ptr);
       // simulator->GetJointMatrix(link_handle[it], link_matrix[it]);
     }
-    float theta=0.0;
+
+    this->solver = new Solver(simulator, joint_handle); 
+    float theta=0.5;
     //< Perform some actions by commanding joint angles(in radians).
-    ForwardKinematics(theta, theta, theta, theta, theta, theta);
+    Controller(theta, theta, theta, theta, theta, theta);
     
     sleep(1);
+
+    Solve();
 
     //< Get positions of objects(joints in this case).
     simxFloat position[3];
     simxFloat orientation[3];
     simxFloat matrix[12];
 
-    simulator->GetObjectPosition(joint_handle[4], origin_handle, position);
-    ChainTransformations(position[0], position[1], position[2]);
+    ChainTransformations();
 
 
     for (int it = 0; it < 6; it++) {
@@ -111,20 +114,7 @@ bool Robot::Initialize(Simulator* simulator) {
   }
 }
 
-void Robot::ForwardKinematics(float t1, float t2, float t3, float t4, float t5,
-                              float t6) {
-  cout << "Starting Froward Kinematics " << endl;
-  simulator->SetJointTargetAngle(joint_handle[0], t1);
-  simulator->SetJointTargetAngle(joint_handle[1], t2);
-  simulator->SetJointTargetAngle(joint_handle[2], t3);
-  simulator->SetJointTargetAngle(joint_handle[3], t4);
-  simulator->SetJointTargetAngle(joint_handle[4], t5);
-  simulator->SetJointTargetAngle(joint_handle[5], t6);
-
-  //------------1 to 2------------//
-}
-
-void Robot::ChainTransformations(float x, float y, float z){
+void Robot::ChainTransformations(){
   cout<<"\nTransfomation matrix chain from Origin to End Effector:"<<endl;
   double t1, t2, t3, t4, t5, t6;
   double a1= -0.0703083;
@@ -137,7 +127,12 @@ void Robot::ChainTransformations(float x, float y, float z){
   double d5= 0.0491754;
   double a5 = 0.0144247;
 
-  t1 = t2 = t3 = t4 = t5 = 0.0;
+  t1 = simulator->GetJointAngle(joint_handle[0]);
+  t2 = simulator->GetJointAngle(joint_handle[1]);
+  t3 = simulator->GetJointAngle(joint_handle[2]);
+  t4 = simulator->GetJointAngle(joint_handle[3]);
+  t5 = simulator->GetJointAngle(joint_handle[4]);
+  t6 = simulator->GetJointAngle(joint_handle[5]);
 
   //Vector3f ot; << 0.3, -0.275, 0.04315;
   Matrix4d T(4,4);
@@ -207,101 +202,35 @@ void Robot::ChainTransformations(float x, float y, float z){
   0, 0, 0, 1;
   T = T*Tn;
   
-  float error = pow( pow( x-T(0,3) ,2 )  + pow(  y-T(1,3), 2 )  + pow( z-T(2,3),2 ) , 0.5 )  ;
+  GetEndEffectorPosition();
+  float error = pow( pow( state.x-T(0,3) ,2 )  + pow(  state.y-T(1,3), 2 )  + pow( state.z-T(2,3),2 ) , 0.5 )  ;
   cout<<"\nTransformation matrix is: \n"<<T <<endl;
   cout<<" Error is: "<<error; 
 
-
-  cout<<"Starting Jacobian inverse motion........ "<<endl;
-  int i=0;
-  while(i<50){
-    i++;
-  sleep(0.1);
-  float dx=0.0000001,dy=0.0,dz=0.0;
-  dx += dx;
-
-  t1 = simulator->GetJointAngle(joint_handle[0]);
-  t2 = simulator->GetJointAngle(joint_handle[1]);
-  t3 = simulator->GetJointAngle(joint_handle[2]);
-  t4 = simulator->GetJointAngle(joint_handle[3]);
-  t5 = simulator->GetJointAngle(joint_handle[4]);
-  t6 = simulator->GetJointAngle(joint_handle[5]);
-
-
-  
-  MatrixXd J(3,4);
-  J(0,0) = (14573216048606699*sin(t1))/576460752303423488 - (7064328361094851*cos(t1)*sin(t2))/18014398509481984 - (7086922019705443*cos(t4)*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)))/144115188075855872 + (7086922019705443*sin(t4)*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)))/144115188075855872 - (6567862346812633*cos(t1)*cos(t2)*sin(t3))/144115188075855872 - (6567862346812633*cos(t1)*cos(t3)*sin(t2))/144115188075855872 ;
-  J(0,1) = (7086922019705443*cos(t4)*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)))/144115188075855872 - (7064328361094851*cos(t2)*sin(t1))/18014398509481984 + (7086922019705443*sin(t4)*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)))/144115188075855872 + (6567862346812633*sin(t1)*sin(t2)*sin(t3))/144115188075855872 - (6567862346812633*cos(t2)*cos(t3)*sin(t1))/144115188075855872 ; 
-  J(0,2) =(7086922019705443*cos(t4)*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)))/144115188075855872 + (7086922019705443*sin(t4)*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)))/144115188075855872 + (6567862346812633*sin(t1)*sin(t2)*sin(t3))/144115188075855872 - (6567862346812633*cos(t2)*cos(t3)*sin(t1))/144115188075855872 ;
-  J(0,3) = (7086922019705443*cos(t4)*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)))/144115188075855872 + (7086922019705443*sin(t4)*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)))/144115188075855872;
-
-  J(1,0) = (7086922019705443*sin(t4)*(sin(t1)*sin(t2)*sin(t3) - cos(t2)*cos(t3)*sin(t1)))/144115188075855872 - (7064328361094851*sin(t1)*sin(t2))/18014398509481984 - (7086922019705443*cos(t4)*(cos(t2)*sin(t1)*sin(t3) + cos(t3)*sin(t1)*sin(t2)))/144115188075855872 - (14573216048606699*cos(t1))/576460752303423488 - (6567862346812633*cos(t2)*sin(t1)*sin(t3))/144115188075855872 - (6567862346812633*cos(t3)*sin(t1)*sin(t2))/144115188075855872;
-  J(1,1) = (7064328361094851*cos(t1)*cos(t2))/18014398509481984 - (7086922019705443*cos(t4)*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)))/144115188075855872 - (6567862346812633*cos(t1)*sin(t2)*sin(t3))/144115188075855872 + (6567862346812633*cos(t1)*cos(t2)*cos(t3))/144115188075855872;
-  J(1,2) = (6567862346812633*cos(t1)*cos(t2)*cos(t3))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)))/144115188075855872 - (6567862346812633*cos(t1)*sin(t2)*sin(t3))/144115188075855872 - (7086922019705443*cos(t4)*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)))/144115188075855872 ;
-  J(1,3) = - (7086922019705443*cos(t4)*(cos(t1)*sin(t2)*sin(t3) - cos(t1)*cos(t2)*cos(t3)))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t1)*cos(t2)*sin(t3) + cos(t1)*cos(t3)*sin(t2)))/144115188075855872 ;
-
-  J(2,0) = 0; 
-  J(2,1) = - (7064328361094851*sin(t2))/18014398509481984 - (6567862346812633*cos(t2)*sin(t3))/144115188075855872 - (6567862346812633*cos(t3)*sin(t2))/144115188075855872 - (7086922019705443*cos(t4)*(cos(t2)*sin(t3) + cos(t3)*sin(t2)))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t2)*cos(t3) - sin(t2)*sin(t3)))/144115188075855872 ;
-  J(2,2) = - (6567862346812633*cos(t2)*sin(t3))/144115188075855872 - (6567862346812633*cos(t3)*sin(t2))/144115188075855872 - (7086922019705443*cos(t4)*(cos(t2)*sin(t3) + cos(t3)*sin(t2)))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t2)*cos(t3) - sin(t2)*sin(t3)))/144115188075855872;
-  J(2,3) = - (7086922019705443*cos(t4)*(cos(t2)*sin(t3) + cos(t3)*sin(t2)))/144115188075855872 - (7086922019705443*sin(t4)*(cos(t2)*cos(t3) - sin(t2)*sin(t3)))/144115188075855872 ;
-  
-
-  MatrixXd Jp(4,3);
-  MatrixXd jtj = J.transpose()*J;
-  MatrixXd jtji = jtj.inverse();
-  MatrixXd jtjijt = jtji*J.transpose();
-  //Jp = ( J.transpose()*J ).inverse() )*J.transpose();
-  Jp = jtjijt;
-  cout<<"Jacobian pseudo inverse: \n"<<Jp<<endl;
-  if (Jp(1,1) != Jp(1,1)){
-    cout<<" Singularity " <<endl;
-    continue;
-  }
-  MatrixXd q(4,1);
-  MatrixXd v(3,1);
-  v<< dx,dy,dz;
-  q = Jp * v;
-  cout<<"Computed joint angles: "<<q.transpose()<<endl<<endl;
-  simulator->SetJointTargetAngle(joint_handle[0], q(0,0));
-  simulator->SetJointTargetAngle(joint_handle[1], q(1,0));
-  simulator->SetJointTargetAngle(joint_handle[2], q(2,0));  
-  simulator->SetJointTargetAngle(joint_handle[3], q(3,0));
-  }
 }
 
-VectorXf Robot::GetEndEffectorPosition() {
-  cout << "Starting Froward Kinematics" << endl;
-
-  vector<double> a;
-  vector<double> d;
-  vector<double> al;
-  vector<double> t;
-  a.push_back(0.2);
-  d.push_back(0.3);
-  t.push_back(0.6);
-  /// Set transformations matrix
-  Matrix4f transformation;
-  int i = 0;
-  // for(int i = 0; i < 5; i++){
-  transformation << cos(t[i]), sin(t[i]), 0, a[i], sin(t[i]), cos(t[i]), 0, 0,
-      0, 0, 1, d[i], 0, 0, 0, 1;
-  //}
-
-  cout << "Transformation matrix: " << endl;
-  Matrix4f tr = transformation;
-  cout << tr(0, 1) << "  " << tr(0, 2);
-
-  Matrix4f tf06;
-
-  VectorXf v(3);
-  // v << tf06(0,3) , tf06(1,3) , tf06(2,3) ;
-  // return v;
+void Robot::GetEndEffectorPosition() {
+    simxFloat position[3];
+    simulator->GetObjectPosition(joint_handle[5], origin_handle, position);
+    state.x = position[0];
+    state.y = position[1];
+    state.z = position[2];
 }
-
-bool Robot::Solver() { return true; }
 
 bool Robot::TrajectoryPlanner() { return true; }
 
-bool Robot::Controller() { return true; }
+bool Robot::Controller(float t1, float t2, float t3, float t4, float t5,
+                              float t6) { 
+  simulator->SetJointTargetAngle(joint_handle[0], t1);
+  simulator->SetJointTargetAngle(joint_handle[1], t2);
+  simulator->SetJointTargetAngle(joint_handle[2], t3);
+  simulator->SetJointTargetAngle(joint_handle[3], t4);
+  simulator->SetJointTargetAngle(joint_handle[4], t5);
+  simulator->SetJointTargetAngle(joint_handle[5], t6);
+  return true; 
+  }
 
-bool Robot::Simulate() { return true; }
+bool Robot::Solve() { 
+  solver->PerformIK();
+  return true; 
+}
