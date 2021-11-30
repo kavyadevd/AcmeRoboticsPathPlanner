4f599f29a0b9b23d3c6be10ac0ffb030d8ad43fc
Kavyashree <kavya27.devadiga@gmail.com>
2021-11-30 14:47:24 -0500

    Update test file

--- test/test.cpp
+++ test/test.cpp
@@ -15,25 +15,14 @@
 #include "simulator.h"  // NOLINT
 #include "solver.h"     // NOLINT
 #include "state.h"      // NOLINT
+#include "arm.hpp"
 
-Simulator simulator;
+// Simulator simulator;
+Arm arm_(1);
 
 /**
  * @brief Tests simulator class
  */
-TEST(TestSimulatorStub, test_simulator) {
-  EXPECT_EQ(simulator.Initialize(), 1);
-  EXPECT_NE(simulator.GetClientID(), -1);
-  char joint[] = "UR5_joint1";
-  simxInt handle = simulator.GetObjectHandle(joint);
-  EXPECT_NE(simulator.GetParent(handle), -1);
-  EXPECT_NE(simulator.GetChild(handle, 0), -1);
-}
-
-/**
- * @brief Tests Robot class
- */
-TEST(TestSimulatorStub, test_robot) {
-  Robot robotic_arm(&simulator);
-  EXPECT_EQ(robotic_arm.Solve(0.1, 0.1, 0.3), true);
+TEST(TestArmStub, test_get_client_id) {
+  EXPECT_NE(arm_.GetClientID(), -1);
 }
