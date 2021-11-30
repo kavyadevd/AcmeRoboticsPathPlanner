ab1a40a1479172b2007b4d272f033beab67b2d31
Kavyashree <kavya27.devadiga@gmail.com>
2021-11-30 14:18:34 -0500

    Update main, call arm method

--- app/main.cpp
+++ app/main.cpp
@@ -18,22 +18,25 @@
 #include "robot.h"      // NOLINT
 #include "simulator.h"  // NOLINT
 #include "solver.h"     // NOLINT
+#include "arm.hpp"
 
 using std::cout;
 using std::endl;
 
 int main() {
-  Simulator simulator;
-  if (!simulator.Initialize()) {
-    return -1;
-  }
-  Robot robot(&simulator);
-  robot.Initialize();
-  std::cout
-      << "\n\nEnter goal x, y, z co-ordinates\n(in meters, values within -1m "
-         "and 1m in all axes):\n ";
-  double goal_x, goal_y, goal_z;
-  std::cin >> goal_x >> goal_y >> goal_z;
-  robot.Solve(goal_x, goal_y, goal_z);
+  Arm* arm_ = new Arm(1);
+  int arm_client = arm_->GetClientID();
+  // Simulator simulator;
+  // if (!simulator.Initialize()) {
+  //   return -1;
+  // }
+  // Robot robot(&simulator);
+  // robot.Initialize();
+  // std::cout
+  //     << "\n\nEnter goal x, y, z co-ordinates\n(in meters, values within -1m "
+  //        "and 1m in all axes):\n ";
+  // double goal_x, goal_y, goal_z;
+  // std::cin >> goal_x >> goal_y >> goal_z;
+  // robot.Solve(goal_x, goal_y, goal_z);
   return 0;
 }
